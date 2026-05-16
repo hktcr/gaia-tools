df305b691be314dce2ef551b4af12e50e0550dd5
Håkan Karlsson <hlg.karlsson@gmail.com>
2026-05-17 00:02:19 +0200

    fix(bin-workshop): Remove hardcoded min-height in chaos-to-clarity to fix slidedeck grid scaling

--- bin-workshop/modules/component-forge.js
+++ bin-workshop/modules/component-forge.js
@@ -4423,7 +4423,7 @@
                 position: relative;
                 width: 100%;
                 height: 100%;
-                min-height: 600px;
+                
                 display: flex;
                 flex-direction: column;
                 align-items: center;
